c5a94ccc853b2aa1f9c648d64af66b030b9b2c21
mrmadhat <danny@websitesbydanny.co.uk>
2017-08-16 08:12:33 +0100

    Enqueueing scripts and styles

--- blockquote-expander.php
+++ blockquote-expander.php
@@ -14,6 +14,20 @@ namespace MrMadHat\BlockquoteExpander;
 require_once('titan-framework/titan-framework-embedder.php');
 use \TitanFramework;
 
+define( __NAMESPACE__ . '\VERSION', '0.1.0' );
+define(__NAMESPACE__ . '\PLUGINURL', plugins_url('', __FILE__) );
+
+add_action('wp_enqueue_scripts', __NAMESPACE__ . '\add_scripts_and_styles');
+
+function add_scripts_and_styles() {
+	wp_register_style( 'bq-expander-style', PLUGINURL . '/assets/dist/css/bq-expander.css', false, VERSION );
+	wp_enqueue_style( 'bq-expander-style' );
+
+	wp_register_script( 'bq-expander-script', PLUGINURL . '/assets/dist/js/bq-expander.min.js', false, '0.1.0', VERSION );
+	wp_enqueue_script( 'bq-expander-script' );
+}
+
+//Testing Titan framework for use in this project
 add_action('tf_create_options', __NAMESPACE__ . '\create_options');
 
 function create_options() {
